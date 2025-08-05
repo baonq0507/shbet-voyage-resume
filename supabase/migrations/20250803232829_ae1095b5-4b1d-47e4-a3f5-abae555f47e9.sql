2523bdc43d168622737f761411112596bc06cf8f
baonq0507 <nquangbao0507@gmail.com>
2025-08-05 03:05:59 +0100

    develop

--- supabase/migrations/20250803232829_ae1095b5-4b1d-47e4-a3f5-abae555f47e9.sql
+++ supabase/migrations/20250803232829_ae1095b5-4b1d-47e4-a3f5-abae555f47e9.sql
@@ -92,33 +92,9 @@ BEGIN
 END;
 $$;
 
-CREATE OR REPLACE FUNCTION public.has_role(_user_id uuid, _role app_role)
-RETURNS boolean
-LANGUAGE sql
-STABLE 
-SECURITY DEFINER
-SET search_path = 'public'
-AS $$
-  SELECT EXISTS (
-    SELECT 1
-    FROM public.user_roles
-    WHERE user_id = _user_id
-      AND role = _role
-  )
-$$;
-
-CREATE OR REPLACE FUNCTION public.get_current_user_role()
-RETURNS app_role
-LANGUAGE sql
-STABLE 
-SECURITY DEFINER
-SET search_path = 'public'
-AS $$
-  SELECT role 
-  FROM public.user_roles 
-  WHERE user_id = auth.uid() 
-  LIMIT 1
-$$;
+-- These functions will be created after the app_role type is available
+-- CREATE OR REPLACE FUNCTION public.has_role(_user_id uuid, _role app_role)
+-- CREATE OR REPLACE FUNCTION public.get_current_user_role()
 
 CREATE OR REPLACE FUNCTION public.handle_new_user_role()
 RETURNS trigger
